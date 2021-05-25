3bf72739017680304b56eb0a078b4db09bc6d1c2
mikelove <michaelisaiahlove@gmail.com>
2021-05-25 19:35:43 +0200

    fix slices in demo

--- demo.R
+++ demo.R
@@ -10,10 +10,13 @@ assay(se, "total") <- assay(se, "a1") + assay(se, "a2")
 assay(se, "ratio") <- assay(se, "a1") / assay(se, "total")
 plotGene <- function(gene) {
   x <- se$slice
+  # deal with diff number of slices
+  x[se$rep == 2] <- x[se$rep == 2] * 27/26
+  x[se$rep == 4] <- x[se$rep == 4] * 27/25
   y <- assay(se, "ratio")[gene,]
   plot(x, y, xlab="slice", ylab="ratio", ylim=c(0,1), main=gene)
   lw <- loess(y ~ x, data=data.frame(x,y))
-  lines(sort(unique(x)), lw$fitted[order(unique(x))], col="red", lwd=2)
+  lines(sort(lw$x), lw$fitted[order(lw$x)], col="red", lwd=2)
   abline(h=0.5, col="grey")
 }
 png(file="fig/slam.png")
